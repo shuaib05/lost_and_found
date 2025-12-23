90934f16ca23baaff1baa4ba25463f55ddea36fc
shuaib05 <shuaib2005f@gmail.com>
2025-12-24 04:06:21 +0530

    Added filter

--- lib/screens/lost_items_screen.dart
+++ lib/screens/lost_items_screen.dart
@@ -10,62 +10,88 @@ class LostItemsScreen extends StatefulWidget {
 }
 
 class _LostItemsScreenState extends State<LostItemsScreen> {
+  bool showonlynotfound = true;
   final List<Item> _items = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Lost Items')),
+      appBar: AppBar(
+        title: Text('Lost Items'),
+        actions: [
+          DropdownButton(
+            value: showonlynotfound ? 'NotFound' : 'All',
+            items: const [
+              DropdownMenuItem(value: 'All', child: Text('Show All')),
+              DropdownMenuItem(
+                value: 'NotFound',
+                child: Text('Show Not Found Only'),
+              ),
+            ],
+            onChanged: (value) {
+              setState(() {
+                showonlynotfound = (value == 'NotFound');
+              });
+            },
+          ),
+        ],
+      ),
       body: _items.isEmpty
           ? const Center(child: Text("No items to show"))
           : ListView.builder(
               itemCount: _items.length,
               itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    title: Text(
-                      _items[index].name,
-                      style: TextStyle(
-                        decoration: _items[index].isFound
-                            ? TextDecoration.lineThrough
-                            : null,
+                if (!showonlynotfound || !_items[index].isFound) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(
+                        _items[index].name,
+                        style: TextStyle(
+                          decoration: _items[index].isFound
+                              ? TextDecoration.lineThrough
+                              : null,
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _items[index].desc,
+                            style: TextStyle(
+                              decoration: _items[index].isFound
+                                  ? TextDecoration.lineThrough
+                                  : null,
+                            ),
+                          ),
+                          Text(
+                            _items[index].num,
+                            style: TextStyle(
+                              decoration: _items[index].isFound
+                                  ? TextDecoration.lineThrough
+                                  : null,
+                            ),
+                          ),
+                          Text(
+                            _items[index].isFound ? "FOUND" : "",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                      leading: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _items[index].isFound = !(_items[index].isFound);
+                          });
+                        },
+                        icon: Icon(
+                          _items[index].isFound
+                              ? Icons.check_box
+                              : Icons.check_box_outline_blank,
+                        ),
+                        color: _items[index].isFound ? Colors.green : null,
                       ),
                     ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _items[index].desc,
-                          style: TextStyle(
-                            decoration: _items[index].isFound
-                                ? TextDecoration.lineThrough
-                                : null,
-                          ),
-                        ),
-                        Text(
-                          _items[index].num,
-                          style: TextStyle(
-                            decoration: _items[index].isFound
-                                ? TextDecoration.lineThrough
-                                : null,
-                          ),
-                        ),
-                      ],
-                    ),
-                    leading: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _items[index].isFound = !(_items[index].isFound);
-                        });
-                      },
-                      icon: Icon(
-                        _items[index].isFound
-                            ? Icons.check_box
-                            : Icons.check_box_outline_blank,
-                      ),
-                      color: _items[index].isFound ? Colors.green : null,
-                    ),
-                  ),
-                );
+                  );
+                }
               },
             ),
       floatingActionButton: FloatingActionButton(
